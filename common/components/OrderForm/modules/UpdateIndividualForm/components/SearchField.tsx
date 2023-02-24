d711615f970bc6cacef295aeba49f1e3eaee1d2e
bohdan radoveniuk <bogdan.radoveniuk@gmail.com>
2023-02-24 13:56:40 +0100

    bugfix

--- common/components/OrderForm/modules/UpdateIndividualForm/components/SearchField.tsx
+++ common/components/OrderForm/modules/UpdateIndividualForm/components/SearchField.tsx
@@ -45,6 +45,8 @@ const SearchField = ({ onSearchResult }: Props) => {
   useEffect(() => {
     if (debouncedSearchValue.length > 3) {
       fetchSearchResult(debouncedSearchValue);
+    } else {
+      setSearchResults([]);
     }
   }, [debouncedSearchValue]);
 
