e5aac9f297fe5bd2a9a5d3434d29f6cf306633a0
bohdan radoveniuk <bogdan.radoveniuk@gmail.com>
2023-03-13 09:26:30 +0100

    debug commit

--- pages/api/get-activities.ts
+++ pages/api/get-activities.ts
@@ -14,20 +14,26 @@ export default async function handler(
   const { id } = req.query;
 
   if (id) {
-    await axios({
-      method: 'GET',
-      url: `https://datahub.ekosystem.slovensko.digital/api/datahub/corporate_bodies/search?q=cin:${id}&expand=rpo_organizations&access_token=${process.env.NEXT_PUBLIC_SLOVENSKO_DIGITAL_PREMIUM_TOKEN}`,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }).then(({ data }) => {
-      res.status(200).json({
-        data: data.rpo_organizations?.[0]?.economic_activity_entries.map((item: any) => ({
-          ...item,
-          status: getActivityStatus(item),
-        })),
+    try {
+      await axios({
+        method: 'GET',
+        url: `https://datahub.ekosystem.slovensko.digital/api/datahub/corporate_bodies/search?q=cin:${id}&expand=rpo_organizations&access_token=${process.env.NEXT_PUBLIC_SLOVENSKO_DIGITAL_PREMIUM_TOKEN}`,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then(({ data }) => {
+        res.status(200).json({
+          data: data.rpo_organizations?.[0]?.economic_activity_entries.map((item: any) => ({
+            ...item,
+            status: getActivityStatus(item),
+          })),
+        });
+      }).catch((err) => {
+        res.status(500).json(err);
       });
-    });
+    } catch (error) {
+      res.status(500).json(error);
+    }
   } else {
     res.status(400).json({ error: 'Id is cannot be empty' });
   }
